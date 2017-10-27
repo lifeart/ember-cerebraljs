a896962dcf4e55888034a6a141f5dc3370acc98c
lifeart <lifeart92@gmail.com>
2017-10-27 03:33:43 +0300

    blueprint update

--- blueprints/ember-cerebraljs/files/app/signals/application.js
+++ blueprints/ember-cerebraljs/files/app/signals/application.js
@@ -6,7 +6,9 @@ function decrease ({state}) {
     state.set('count', state.get('count') - 1)
 }
 
-export default const signals {
+const signals = {
     onIncrease: [increase],
     onDecrease: [decrease]
 }
+
+export default signals;
