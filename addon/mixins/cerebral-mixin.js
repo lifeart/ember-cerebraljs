3aca241c0141a0ab8ae65f1d5ff502fa9436d2f9
lifeart <lifeart92@gmail.com>
2017-10-28 21:29:22 +0300

    empty data errors

--- addon/mixins/cerebral-mixin.js
+++ addon/mixins/cerebral-mixin.js
@@ -1,6 +1,12 @@
 import Mixin from '@ember/object/mixin';
-import { computed, get, set } from '@ember/object';
-import {inject} from '@ember/service';
+import {
+  computed,
+  get,
+  set
+} from '@ember/object';
+import {
+  inject
+} from '@ember/service';
 import normalizeSignalName from '../utils/signal-normalizer';
 export default Mixin.create({
   cerebraljs: inject(),
@@ -8,24 +14,29 @@ export default Mixin.create({
     return false;
   }),
   init() {
-    this.set('cerebral', get(this,'cerebraljs').get('cerebral'));
+    set(this,'cerebral', get(this, 'cerebraljs').get('cerebral'));
     this.bindProps(this.cerebralProps());
     this.subscribePropsToCerebralUpdates(this.cerebralProps());
     this._super(...arguments);
   },
   cerebralProps() {
     const propsObject = {};
-    this.get('props').forEach((propName)=>{
-        const [key, value = key] = propName.split(':');
-        propsObject[key] = value;
+    this.get('props').forEach((propName) => {
+      const [key, value = key] = propName.split(':');
+      propsObject[key] = value;
     });
     return propsObject;
   },
   bindProps(cerebralProps) {
-    const cerebral = get(this,'cerebral');
+    const cerebral = get(this, 'cerebral');
     this._eachInObject(cerebralProps, (prop, path) => {
       set(this, prop, computed(() => {
-        return cerebral.getState(path || prop);
+        const pathName = path || prop;
+        const result = cerebral.getState(pathName);
+        if (typeof result === 'undefined') {
+          console.error(`Unable to get state for property "${prop}" by path "${pathName}"`, this, cerebral);
+        }
+        return result;
       }).readOnly());
     });
   },
@@ -50,11 +61,11 @@ export default Mixin.create({
   },
 
   cursorsByProp(cerebralProps) {
-    const cerebral = get(this,'cerebral');
+    const cerebral = get(this, 'cerebral');
     const memo = {};
     const state = cerebral.getModel().state;
     this._eachInObject(cerebralProps, (prop, path) => {
-      memo[prop] = state.select.apply(state,path.split('.'));
+      memo[prop] = state.select.apply(state, path.split('.'));
     });
     return memo;
   },
@@ -65,9 +76,13 @@ export default Mixin.create({
     };
   },
 
-  sendSignal(name,...props) {
-      const signal = get(this,'cerebral').getSignal(normalizeSignalName(name));
-      signal.apply(signal, props);
+  sendSignal(name, ...props) {
+    const signal = get(this, 'cerebral').getSignal(normalizeSignalName(name));
+    if (!signal) {
+      console.error(`Unable to find signal "${name}"`, props, this);
+      return;
+    }
+    signal.apply(signal, props);
   },
 
   didDestroyElement() {
